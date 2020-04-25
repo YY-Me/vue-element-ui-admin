ef731844be1ba652471cc4ba7a06179536b0efd1
yy <1396513066@qq.com>
2020-04-26 07:53:42 +0800

    后台

---
+++ boot-auth/src/main/java/vip/bblog/boot/auth/BootAuthApplication.java
@@ -0,0 +1,18 @@
+package vip.bblog.boot.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yy
+ * @version 1.0
+ * @date 2020-1-23
+ */
+@SpringBootApplication
+public class BootAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAuthApplication.class, args);
+    }
+
+}
---
+++ boot-auth/src/test/java/vip/bblog/boot/auth/BootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.bblog.boot.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
