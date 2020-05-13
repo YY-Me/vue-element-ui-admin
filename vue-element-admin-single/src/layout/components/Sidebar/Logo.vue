47ce72c492bb042f9d9250886c1ab204f83838ee
yuyong <1396513066@qq.com>
2020-05-13 18:08:12 +0800

    配置修改

--- vue-element-admin-single/src/layout/components/Sidebar/Logo.vue
+++ vue-element-admin-single/src/layout/components/Sidebar/Logo.vue
@@ -3,7 +3,7 @@
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="miniLogo" class="sidebar-mini-logo" alt="权限管理系统">
-        <h1 v-else class="sidebar-title">{{ title }} </h1>
+        <!--<h1 v-else class="sidebar-title">{{ title }} </h1>-->
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo" alt="权限管理系统">
